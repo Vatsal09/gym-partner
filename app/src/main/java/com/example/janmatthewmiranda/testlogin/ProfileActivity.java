e448d4a4e77c55035d2378d8b6ca13ea0cf95be7
Jan Matthew Miranda <janmatthewmiranda@Jans-MacBook-Pro.local>
2017-12-12 13:39:47 -0500

    Added framelayout for main activity page

--- app/src/main/java/com/example/janmatthewmiranda/testlogin/ProfileActivity.java
+++ app/src/main/java/com/example/janmatthewmiranda/testlogin/ProfileActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -13,8 +15,12 @@ import com.google.firebase.auth.FirebaseUser;
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
-    private TextView emailText;
+//    private TextView emailText;
     private Button logoutBtn;
+    private Button profileBtn;
+    private Button homeBtn;
+    private Button matchesBtn;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,20 +36,39 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
+        profileBtn = (Button) findViewById(R.id.profileBtn);
+        homeBtn = (Button) findViewById(R.id.homeBtn);
+        matchesBtn = (Button) findViewById(R.id.matchesBtn);
+        frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
         logoutBtn = (Button) findViewById(R.id.logoutBtn);
-        emailText = (TextView) findViewById(R.id.emailText);
 
-        emailText.setText("Welcome " + user.getEmail());
+//        emailText = (TextView) findViewById(R.id.emailText);
+        Toast.makeText(getBaseContext(), "Welcome " + user.getEmail(), Toast.LENGTH_SHORT).show();
+
+//        emailText.setText("Welcome " + user.getEmail());
         logoutBtn.setOnClickListener(this);
+        profileBtn.setOnClickListener(this);
+        homeBtn.setOnClickListener(this);
+        matchesBtn.setOnClickListener(this);
+
 
     }
 
     @Override
     public void onClick(View v) {
+        if(v == profileBtn) {
+            //set frameLayout accordingly
+        }
+        if(v == homeBtn) {
+            //set frameLayout accordingly
+        }
+        if(v == matchesBtn) {
+            //set frameLayout accordingly
+        }
         if(v == logoutBtn) {
             firebaseAuth.signOut();
             finish();
             startActivity(new Intent(this, LoginActivity.class));
-        }
+      }
     }
 }
