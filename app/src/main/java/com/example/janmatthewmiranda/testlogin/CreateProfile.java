9a6fbec8ce95ab10ed001f0df6790148b7f487bf
nilesh <np_swo@yahoo.com>
2017-12-16 22:57:50 -0500

    CreateProfile changed

--- app/src/main/java/com/example/janmatthewmiranda/testlogin/CreateProfile.java
+++ app/src/main/java/com/example/janmatthewmiranda/testlogin/CreateProfile.java
@@ -1,11 +1,20 @@
 package com.example.janmatthewmiranda.testlogin;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -13,12 +22,38 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.IgnoreExtraProperties;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class CreateProfile extends AppCompatActivity implements View.OnClickListener{
 
+    public static final int GALLERY_REQUEST = 95;
     private Button saveBtn;
     private EditText name;
     private EditText age;
     private EditText phoneNumber;
+    private Spinner spinner1;
+    private Button uploadBtn;
+    private ImageView imageDisplay;
+    private String genderSelected;
+
+    private int progress1 = 0;
+    private int progress2 = 0;
+    private int progress3 = 0;
+    private int progress4 = 0;
+    private int progress5 = 0;
+
+    private SeekBar seekBar1;
+    private SeekBar seekBar2;
+    private SeekBar seekBar3;
+    private SeekBar seekBar4;
+    private SeekBar seekBar5;
+
+
+
+
+
 
     private FirebaseAuth firebaseAuth;
     private FirebaseDatabase database;
@@ -39,6 +74,29 @@ public class CreateProfile extends AppCompatActivity implements View.OnClickList
         }
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
+        spinner1 = (Spinner) findViewById(R.id.genderSpinner);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                genderSelected = adapterView.getItemAtPosition(i).toString();
+                System.out.println(genderSelected);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        uploadBtn = (Button) findViewById(R.id.uploadBtn);
+        imageDisplay = (ImageView) findViewById(R.id.seeImage);
+
+        seekBar1 = (SeekBar) findViewById(R.id.seekBar_1);
+        seekBar2 = (SeekBar) findViewById(R.id.seekBar_2);
+        seekBar3 = (SeekBar) findViewById(R.id.seekBar_3);
+        seekBar4 = (SeekBar) findViewById(R.id.seekBar_4);
+        seekBar5 = (SeekBar) findViewById(R.id.seekBar_5);
+        setSeekBars();
+
 
         saveBtn = (Button) findViewById(R.id.saveBtn);
         saveBtn.setOnClickListener(this);
@@ -51,6 +109,8 @@ public class CreateProfile extends AppCompatActivity implements View.OnClickList
         name = (EditText) findViewById(R.id.name);
         age = (EditText) findViewById(R.id.age);
         phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+
+
         user = firebaseAuth.getCurrentUser();
 
         String nName = name.getText().toString();
@@ -94,5 +154,138 @@ public class CreateProfile extends AppCompatActivity implements View.OnClickList
         if (v == saveBtn) {
             saveUser();
         }
+    }
+
+    // Opens the gallery using implicit intent
+    public void openGallery(View v){
+        Intent getImagefromGallery = new Intent(Intent.ACTION_PICK);
+        File galleryDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String path = galleryDir.getPath();
+        Uri url = Uri.parse(path);
+        getImagefromGallery.setDataAndType(url, "image/*");
+        startActivityForResult(getImagefromGallery, GALLERY_REQUEST);
+
+
+    }
+
+    public void setSeekBars(){
+        seekBar1.setMax(100);
+        seekBar2.setMax(100);
+        seekBar3.setMax(100);
+        seekBar4.setMax(100);
+        seekBar5.setMax(100);
+
+        seekBar1.setProgress(progress1);
+        seekBar2.setProgress(progress2);
+        seekBar3.setProgress(progress3);
+        seekBar4.setProgress(progress4);
+        seekBar5.setProgress(progress5);
+
+
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progress1 = i;
+                System.out.println(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progress2 = i;
+                System.out.println(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progress3 = i;
+                System.out.println(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar4.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progress4 = i;
+                System.out.println(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar5.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progress5 = i;
+                System.out.println(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(requestCode == GALLERY_REQUEST){
+                Uri uriOfImage = data.getData();
+                InputStream inputStream;
+                try {
+                    inputStream=getContentResolver().openInputStream(uriOfImage);
+                    Bitmap imageToUpload = BitmapFactory.decodeStream(inputStream);
+                    imageDisplay.setImageBitmap(imageToUpload);
+                }catch (FileNotFoundException exc){
+                    exc.printStackTrace();
+                    Toast.makeText(this, "Could not open image. Please choose another image",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
     }
 }
